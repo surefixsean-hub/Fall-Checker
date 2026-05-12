7f0d909aaa8fb788ea66604a0818347e0862480b
surefixsean-hub <surefixsean@gmail.com>
2026-05-13 08:49:22 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// Simple cache-first service worker for offline use
+const CACHE = 'fall-checker-web-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
